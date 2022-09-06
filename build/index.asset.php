60d22f259ff841eb0c42df6e75600c24ed2d713f
Nick Diego <ndiego@outermost.co>
2022-09-05 20:02:39 -0500

    Fresh build.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '3370018d319a6b54ce37');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '633d04d4886f1d5f87bd');
